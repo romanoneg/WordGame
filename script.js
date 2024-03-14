6680969c0ef12bdfe85c1aaff28de8b54bfa566b
R <romanoneg@Rs-MacBook-Air.lan>
2024-03-13 23:46:44 -0400

    Version 0.1

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Global variable to hold the model
 let model = null;
 
+const wordList = ["exercise", "court", "defeat", "admire","dead", "attraction", 
+  "hospital","generation", "tempt", "museum", "flower","garlic", "notorious", 
+  "foster","comfortable", "character", "cookie", "innocent","height", "valid", "virtue", 
+  "sunrise","construct","potential", "large","sweep", "sunshine", "resource","bite", "money", 
+  "sentence","grief","minute", "similar", "reality","grind", "purpose","delicate","suntan", 
+  "financial", "rage","confession","implication","shorts", "moral","strange", "spell","mayor",
+  "demonstrator","oil", "cat", "beautiful", "princess"
+];
+
+
 console.log("Starting")
 
 // Load the model when the page loads
@@ -40,12 +50,19 @@ async function calculateDissimilarity() {
     }
 
     const dissimilarity = similarities.length > 0 ?
-                          1 - (sum(similarities) / similarities.length) :
+                          (1 - (sum(similarities) / similarities.length)) / 0.8 :
                           0;
 
     $("#result").text((dissimilarity * 100).toFixed(2) + "% dissimilar!");
 
   }); 
+}
+
+function challengeMe() {
+  const randomIndex = Math.floor(Math.random() * wordList.length);
+  const randomWord = wordList[randomIndex];
+  document.getElementById("word5").value = randomWord;
+  document.getElementById("word5").readOnly = true;
 }
 
 // Helper function to calculate cosine similarity between TensorFlow.js tensors
